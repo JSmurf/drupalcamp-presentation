b4014ac9b6d1c2729731f893d1048782361bd7f9
Alex <mrsketchygalore@me.com>
2019-10-01 17:15:56 -0400

    Removed link to IWP Talk
    Didn't make slides for this... So I'm deleting it.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,7 @@ const IndexPage = () => (
     <h1>Alex Cooper's Gatsby Site</h1>
     <p>Welcome to Alex Cooper's Gatsby site.</p>
     <p>View the <Link to="/umami">Umami Site</Link></p>
-    <p>View the <Link to="/drupalcamp">DrupalCamp Presentation</Link></p>
-    <p>View the <Link to="/ithacawebpeople">Ithaca Web People Presentation</Link></p>
+    <p>View the <Link to="/drupalcamp">Presentation</Link></p>
     <p>View the <a href="https://github.com/JSmurf/drupalcamp-presentation">GitHub Repo</a></p>
   </Layout>
 )
